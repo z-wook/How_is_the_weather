3bb6beb21d74ee55101e5bb5862942c46df5db00
suojae3 <suojae3@gmail.com>
2023-09-25 16:56:30 +0900

    [FEAT] APIManager세팅

---
+++ How_is_the_weather/Scenes/VCModel/ViewControllerModel.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+protocol ViewControllerModelDelegate: AnyObject {
+    func didFetchWeather(weather: Weather)
+    func didFailToFetchWeather(error: Error)
+}
+
+class ViewControllerModel {
+    
+    weak var delegate: ViewControllerModelDelegate?
+
+    func fetchWeatherForCity(_ city: String) {
+        ApiManager.shared.fetchWeather(forCity: city) { [weak self] result in
+            switch result {
+            case .success(let weather):
+                self?.delegate?.didFetchWeather(weather: weather)
+            case .failure(let error):
+                self?.delegate?.didFailToFetchWeather(error: error)
+            }
+        }
+    }
+}

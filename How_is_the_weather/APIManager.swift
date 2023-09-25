a8a8388f8107d1217275d1acbadd15e589a3947f
suojae3 <suojae3@gmail.com>
2023-09-25 12:41:34 +0900

    [FEAT] Fetch Weather Data

---
+++ How_is_the_weather/APIManager.swift
@@ -0,0 +1,27 @@
+import Alamofire
+import SwiftyJSON
+
+class ApiManager {
+
+    static let shared = ApiManager()
+    private let apiKey = "e050990db22df2ab58ab3c620741e32e"
+    private let baseUrl = "https://api.openweathermap.org/data/2.5/weather"
+
+    private init() {}
+
+    func getWeather(forCity city: String, completion: @escaping (Result<JSON, AFError>) -> Void) {
+        let parameters: [String: Any] = [
+            "q": city,
+            "appid": apiKey
+        ]
+
+        AF.request(baseUrl, parameters: parameters).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                completion(.success(JSON(value)))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
--- How_is_the_weather/ViewController.swift
+++ How_is_the_weather/ViewController.swift
@@ -1,19 +1,45 @@
-//
-//  ViewController.swift
-//  How_is_the_weather
-//
-//  Copyright (c) 2023 z-wook. All right reserved.
-//
-
 import UIKit
+import SwiftyJSON
 
+
+//MARK: - Properties & Deinit
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    deinit {
+        print("ViewController deinitialize!!")
+    }
+}
+ 
+//MARK: - Lifecycle
+extension ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fetchWeatherForCity("Seoul")
     }
-
-
 }
 
+
+//MARK: - Fetch Weather with update
+extension ViewController {
+    private func fetchWeatherForCity(_ city: String) {
+        ApiManager.shared.getWeather(forCity: city) { [weak self] result in
+            guard let strongSelf = self else { return }
+            
+            switch result {
+            case .success(let json):
+                strongSelf.updateUIWithWeatherInfo(json)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func updateUIWithWeatherInfo(_ json: JSON) {
+        let weatherDescription = json["weather"][0]["description"].stringValue
+        DispatchQueue.main.async {
+            self.weatherLabel.text = weatherDescription
+        }
+    }
+}

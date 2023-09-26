9afc5156302eadd553361de1325bccc79b4e66e7
HAN JIWOOK <jiwook.han.dev@gmail.com>
2023-09-26 10:05:22 +0900

    [FEAT] : ⚙️ SearchView 임시 완료

---
+++ How_is_the_weather/Scenes/View/SearchView.swift
@@ -0,0 +1,8 @@
+//
+//  SearchView.swift
+//  How_is_the_weather
+//
+//  Copyright (c) 2023 z-wook. All right reserved.
+//
+
+import Foundation
---
+++ How_is_the_weather/Scenes/View/WeatherCell.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherCell.swift
+//  How_is_the_weather
+//
+//  Copyright (c) 2023 z-wook. All right reserved.
+//
+
+import Foundation

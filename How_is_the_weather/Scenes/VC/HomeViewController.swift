8cedfe6495fc5bb788c5e85bfb3bbee037fda724
junyoung_hong <wnsdud0721@naver.com>
2023-09-27 20:08:47 +0900

    FEATURE: GPS class 생성

--- How_is_the_weather/Scenes/VC/HomeViewController.swift
+++ How_is_the_weather/Scenes/VC/HomeViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
+import CoreLocation
 
 final class HomeViewController: UIViewController {
     
+    let gpsController = GPSController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        gpsController.setLocationManager()
+        print()
     }
     
     deinit {

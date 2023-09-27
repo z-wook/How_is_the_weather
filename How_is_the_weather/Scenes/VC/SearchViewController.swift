26f9d3e3dbe9e4a35de10ed802bf16437f1fc029
jiwook_han <jiwook.han.dev@gmail.com>
2023-09-27 14:41:25 +0900

    [MERGE] : ✅ conflict 해결

--- How_is_the_weather/Scenes/VC/SearchViewController.swift
+++ How_is_the_weather/Scenes/VC/SearchViewController.swift
@@ -28,10 +28,6 @@ final class SearchViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
     }
     
     deinit {
@@ -107,9 +103,4 @@ extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSo
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.bounds.width, height: 100)
     }
-    
-    
-    
-    
-    
 }

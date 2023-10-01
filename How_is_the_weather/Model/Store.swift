606e826f71d7c06f573572430f119aa996da0a93
이애라(7_iOS) <139456385+aera11@users.noreply.github.com>
2023-10-01 22:51:13 +0900

    [FEAT]:파일수정

---
+++ How_is_the_weather/Model/Store.swift
@@ -0,0 +1,13 @@
+//
+//  Store.swift
+//  How_is_the_weather
+//
+//  Created by t2023-m0095 on 2023/10/01.
+//
+
+import Foundation
+
+struct Store {
+    let API_KEY = "e050990db22df2ab58ab3c620741e32e"
+}
+

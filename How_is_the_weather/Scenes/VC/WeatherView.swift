6ff6e44a22ca9955651351de9fab716d9c72eaf7
이애라(7_iOS) <139456385+aera11@users.noreply.github.com>
2023-10-01 22:52:40 +0900

    [FEAT]:에러수정

--- How_is_the_weather/Scenes/VC/WeatherView.swift
+++ How_is_the_weather/Scenes/VC/WeatherView.swift
@@ -32,8 +32,7 @@ class WeatherView : UIView {
         makeCity()
         setlayout()
     }
-    func configure(data:Weather) {
-    }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")

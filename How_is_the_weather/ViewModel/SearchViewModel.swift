4151229dca3b2683c0032b1b04ecee0a42fd6f6b
suojae3 <suojae3@gmail.com>
2023-10-02 16:21:23 +0900

    [FIX] 파일 뒤바뀜 오류 수정

--- How_is_the_weather/ViewModel/SearchViewModel.swift
+++ How_is_the_weather/ViewModel/SearchViewModel.swift
@@ -1,44 +1,83 @@
+
 import Foundation
 
-protocol WeatherViewModelDelegate: AnyObject {
-    func didFetchWeather(weather: Weather)
-    func didFailToFetchWeather(error: Error)
+final class SearchViewModel {
+    let manager = WeatherViewModel()
+    private let weatherKey = "weather"
+    var weatherList: [WeatherInfo?] = []
+    var textFieldText: String? = ""
+    var reloadCollectionView: (() -> Void)?
 }
 
-class WeatherViewModel {
-    
-    weak var delegate: WeatherViewModelDelegate?
-    
-    private let apiManager: APIManager
-    private var weatherData: Weather?
-    
-    var temperatureText: String {
-        return "\(weatherData?.temperature ?? 0)°C"
+extension SearchViewModel {
+    var loadWeatherList: Void {
+        let cityList = fetchCityList
+        
+        for city in cityList {
+            weatherList.append(WeatherInfo(city: city))
+            manager.fetchWeatherForCity(city)
+        }
     }
     
-    var cityName: String {
-        return weatherData?.name ?? ""
+    var searchWeather: Void {
+        guard let text = textFieldText else { return }
+        manager.fetchWeatherForCity(text)
     }
     
-    var weatherID: Int? {
-        return weatherData?.id
-    }
-
-    init(apiManager: APIManager = APIManager()) {
-        self.apiManager = apiManager
-    }
-    
-    func fetchWeatherForCity(_ city: String) {
-        apiManager.fetchWeather(forCity: city) { [weak self] result in
-            guard let self = self else { return }
-
-            switch result {
-            case .success(let weather):
-                self.weatherData = weather
-                self.delegate?.didFetchWeather(weather: weather)
-            case .failure(let error):
-                self.delegate?.didFailToFetchWeather(error: error)
+    func receiveWeather(weather: Weather) {
+        if let index = getIndex(city: weather.name) {
+            weatherList[index]?.id = weather.id
+            weatherList[index]?.description = weather.description
+            weatherList[index]?.temperature = weather.temperature
+            reloadCollectionView?()
+            return
+        }
+        let city = weather.name
+        if checkDuplication(city: city) == false {
+            let info = WeatherInfo(id: weather.id, city: city, description: weather.description, temperature: weather.temperature)
+            weatherList.append(info)
+            reloadCollectionView?()
+            
+            let cityList = weatherList.compactMap { weather in
+                weather?.city
             }
+            saveCityList(list: cityList)
+        }
+    }
+    
+    func removeWeather(index: IndexPath) {
+        weatherList.remove(at: index.item)
+        reloadCollectionView?()
+        
+        var list = fetchCityList
+        list.remove(at: index.item)
+        saveCityList(list: list)
+    }
+}
+
+private extension SearchViewModel {
+    func saveCityList(list: [String]) {
+        let encoder = JSONEncoder()
+        if let encodedData = try? encoder.encode(list) {
+            UserDefaultsManager.setValue(value: encodedData, key: weatherKey)
+        }
+    }
+    
+    var fetchCityList: [String] {
+        let decoder = JSONDecoder()
+        guard let data: Data = UserDefaultsManager.getValue(key: weatherKey),
+              let decodedData = try? decoder.decode([String].self, from: data) else { return [] }
+        return decodedData
+    }
+    
+    func getIndex(city: String) -> Int? {
+        return fetchCityList.firstIndex(of: city)
+    }
+    
+    func checkDuplication(city: String) -> Bool {
+        return weatherList.contains {
+            $0?.city == city
         }
     }
 }
+
--- How_is_the_weather/ViewModel/WeatherViewModel.swift
+++ How_is_the_weather/ViewModel/WeatherViewModel.swift
@@ -1,88 +1,45 @@
 //
-//  SearchViewModel.swift
-//  How_is_the_weather
-//
-//  Copyright (c) 2023 z-wook. All right reserved.
-//
-
 import Foundation
 
-final class SearchViewModel {
-    let manager = WeatherViewModel()
-    private let weatherKey = "weather"
-    var weatherList: [WeatherInfo?] = []
-    var textFieldText: String? = ""
-    var reloadCollectionView: (() -> Void)?
+protocol WeatherViewModelDelegate: AnyObject {
+    func didFetchWeather(weather: Weather)
+    func didFailToFetchWeather(error: Error)
 }
 
-extension SearchViewModel {
-    var loadWeatherList: Void {
-        let cityList = fetchCityList
-        
-        for city in cityList {
-            weatherList.append(WeatherInfo(city: city))
-            manager.fetchWeatherForCity(city)
-        }
+class WeatherViewModel {
+    
+    weak var delegate: WeatherViewModelDelegate?
+    
+    private let apiManager: APIManager
+    private var weatherData: Weather?
+    
+    var temperatureText: String {
+        return "\(weatherData?.temperature ?? 0)°C"
     }
     
-    var searchWeather: Void {
-        guard let text = textFieldText else { return }
-        manager.fetchWeatherForCity(text)
+    var cityName: String {
+        return weatherData?.name ?? ""
     }
     
-    func receiveWeather(weather: Weather) {
-        if let index = getIndex(city: weather.name) {
-            weatherList[index]?.id = weather.id
-            weatherList[index]?.description = weather.description
-            weatherList[index]?.temperature = weather.temperature
-            reloadCollectionView?()
-            return
-        }
-        let city = weather.name
-        if checkDuplication(city: city) == false {
-            let info = WeatherInfo(id: weather.id, city: city, description: weather.description, temperature: weather.temperature)
-            weatherList.append(info)
-            reloadCollectionView?()
-            
-            let cityList = weatherList.compactMap { weather in
-                weather?.city
+    var weatherID: Int? {
+        return weatherData?.id
+    }
+
+    init(apiManager: APIManager = APIManager()) {
+        self.apiManager = apiManager
+    }
+    
+    func fetchWeatherForCity(_ city: String) {
+        apiManager.fetchWeather(forCity: city) { [weak self] result in
+            guard let self = self else { return }
+
+            switch result {
+            case .success(let weather):
+                self.weatherData = weather
+                self.delegate?.didFetchWeather(weather: weather)
+            case .failure(let error):
+                self.delegate?.didFailToFetchWeather(error: error)
             }
-            saveCityList(list: cityList)
-        }
-    }
-    
-    func removeWeather(index: IndexPath) {
-        weatherList.remove(at: index.item)
-        reloadCollectionView?()
-        
-        var list = fetchCityList
-        list.remove(at: index.item)
-        saveCityList(list: list)
-    }
-}
-
-private extension SearchViewModel {
-    func saveCityList(list: [String]) {
-        let encoder = JSONEncoder()
-        if let encodedData = try? encoder.encode(list) {
-            UserDefaultsManager.setValue(value: encodedData, key: weatherKey)
-        }
-    }
-    
-    var fetchCityList: [String] {
-        let decoder = JSONDecoder()
-        guard let data: Data = UserDefaultsManager.getValue(key: weatherKey),
-              let decodedData = try? decoder.decode([String].self, from: data) else { return [] }
-        return decodedData
-    }
-    
-    func getIndex(city: String) -> Int? {
-        return fetchCityList.firstIndex(of: city)
-    }
-    
-    func checkDuplication(city: String) -> Bool {
-        return weatherList.contains {
-            $0?.city == city
         }
     }
 }

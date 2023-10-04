0e29f767ea0812aa22ace7452c68c4b6f1ebaa1d
이애라(7_iOS) <139456385+aera11@users.noreply.github.com>
2023-10-04 14:43:32 +0900

    [FEAT]:label -> button 변경

--- How_is_the_weather/ViewController/WeatherView.swift
+++ How_is_the_weather/ViewController/WeatherView.swift
@@ -3,9 +3,7 @@
 //  How_is_the_weather
 //
 //  Created by t2023-m0095 on 2023/09/26.
-//
 
-import Foundation
 import UIKit
 import SnapKit
 
@@ -17,16 +15,57 @@ import SnapKit
 //    self.view.addSubview(shadows)
 
 class WeatherView : UIViewController {
-    var temperature = UILabel()
+    var temperature = UIButton(type: .system)
     var city = UILabel()
+    let thunderstormImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "thunderstorm")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return imageView
+    }()
+    let drizzleImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "drizzle")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return imageView
+    }()
+    let rainImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "rain")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return imageView
+    }()
+    let snowImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "snow")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return imageView
+    }()
+    let atmosphereImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "atmosphere")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return imageView
+    }()
+    let tornadoImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "tornado")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return imageView
+    }()
     let sunImageView : UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "sun")
         imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
         return imageView
     }()
+    let cloudsImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "clouds")
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        return imageView
+    }()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setlayout()
@@ -36,9 +75,10 @@ class WeatherView : UIViewController {
     }
     
     func makeTemperature() {
-        temperature.textColor = .black
-        temperature.font = .systemFont(ofSize: 110)
-        temperature.text = "10"
+        temperature.setTitle("10", for: .normal)
+        temperature.titleLabel?.font = .systemFont(ofSize: 100)
+        temperature.backgroundColor = .none
+        temperature.frame = CGRect(x: 400, y: 400, width: 300, height: 300)
     }
     func makeCity() {
         city.textColor = .black
@@ -52,7 +92,7 @@ class WeatherView : UIViewController {
         
         temperature.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(130)
-            make.left.equalToSuperview().offset(80)
+            make.left.equalToSuperview().offset(100)
         }
         city.snp.makeConstraints { make in
             make.top.equalTo(temperature.snp.bottom)
